1f803dd059a077f42be868752091ad8d44fb5195
payapula <bharathikannank05@gmail.com>
2021-08-21 13:56:12 +0530

    Added Roboto Slab font, created _document file.

---
+++ pages/_document.tsx
@@ -0,0 +1,27 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { ReactElement } from 'react';
+
+export default class MyDocument extends Document {
+    render(): ReactElement {
+        return (
+            <Html lang="en">
+                <Head>
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link
+                        rel="preconnect"
+                        href="https://fonts.gstatic.com"
+                        crossOrigin="anonymous"
+                    />
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@400;700&display=swap"
+                        rel="stylesheet"
+                    />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
